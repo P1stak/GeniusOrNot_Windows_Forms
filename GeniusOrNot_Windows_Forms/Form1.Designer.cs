91d587bac03c983b7c1156348f9237751e036ce5
Александр Pistak <lutcifer333@gmail.com>
2025-04-26 15:46:19 +0700

    Add project files.

---
+++ GeniusOrNot_Windows_Forms/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using System.Windows.Forms;
+
+namespace GeniusOrNot_Windows_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelName = new System.Windows.Forms.Panel();
+            this.btnStartTest = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelResults = new System.Windows.Forms.Panel();
+            this.btnShowResults = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panelQuestions = new System.Windows.Forms.Panel();
+            this.btnSubmitAnswer = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.panelName.SuspendLayout();
+            this.panelResults.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panelQuestions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelName
+            // 
+            this.panelName.Controls.Add(this.btnStartTest);
+            this.panelName.Controls.Add(this.txtName);
+            this.panelName.Controls.Add(this.label1);
+            this.panelName.Location = new System.Drawing.Point(0, 0);
+            this.panelName.Name = "panelName";
+            this.panelName.Size = new System.Drawing.Size(401, 600);
+            this.panelName.TabIndex = 0;
+            // 
+            // btnStartTest
+            // 
+            this.btnStartTest.Location = new System.Drawing.Point(159, 332);
+            this.btnStartTest.Name = "btnStartTest";
+            this.btnStartTest.Size = new System.Drawing.Size(98, 37);
+            this.btnStartTest.TabIndex = 5;
+            this.btnStartTest.Text = "Начать тестирование";
+            this.btnStartTest.UseVisualStyleBackColor = true;
+            this.btnStartTest.Click += new System.EventHandler(this.btnStartTest_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(159, 258);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(200, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите ваше ФИО:";
+            // 
+            // panelResults
+            // 
+            this.panelResults.Controls.Add(this.btnShowResults);
+            this.panelResults.Controls.Add(this.dataGridView1);
+            this.panelResults.Location = new System.Drawing.Point(0, 0);
+            this.panelResults.Name = "panelResults";
+            this.panelResults.Size = new System.Drawing.Size(401, 600);
+            this.panelResults.TabIndex = 2;
+            this.panelResults.Visible = false;
+            // 
+            // btnShowResults
+            // 
+            this.btnShowResults.Location = new System.Drawing.Point(144, 185);
+            this.btnShowResults.Name = "btnShowResults";
+            this.btnShowResults.Size = new System.Drawing.Size(133, 23);
+            this.btnShowResults.TabIndex = 1;
+            this.btnShowResults.Text = "Показать результаты";
+            this.btnShowResults.UseVisualStyleBackColor = true;
+            this.btnShowResults.Click += new System.EventHandler(this.btnShowResults_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(401, 600);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panelQuestions
+            // 
+            this.panelQuestions.Controls.Add(this.btnSubmitAnswer);
+            this.panelQuestions.Controls.Add(this.txtAnswer);
+            this.panelQuestions.Controls.Add(this.lblQuestion);
+            this.panelQuestions.Location = new System.Drawing.Point(0, 0);
+            this.panelQuestions.Name = "panelQuestions";
+            this.panelQuestions.Size = new System.Drawing.Size(401, 600);
+            this.panelQuestions.TabIndex = 1;
+            this.panelQuestions.Visible = false;
+            // 
+            // btnSubmitAnswer
+            // 
+            this.btnSubmitAnswer.Location = new System.Drawing.Point(159, 144);
+            this.btnSubmitAnswer.Name = "btnSubmitAnswer";
+            this.btnSubmitAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmitAnswer.TabIndex = 2;
+            this.btnSubmitAnswer.Text = "Ответить";
+            this.btnSubmitAnswer.UseVisualStyleBackColor = true;
+            this.btnSubmitAnswer.Click += new System.EventHandler(this.btnSubmitAnswer_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(144, 74);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Location = new System.Drawing.Point(0, 0);
+            this.lblQuestion.MaximumSize = new System.Drawing.Size(400, 60);
+            this.lblQuestion.MinimumSize = new System.Drawing.Size(0, 60);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(389, 60);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Вопрос будет здесь";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 600);
+            this.Controls.Add(this.panelName);
+            this.Controls.Add(this.panelQuestions);
+            this.Controls.Add(this.panelResults);
+            this.Name = "Form1";
+            this.Text = "Тест на гениальность";
+            this.panelName.ResumeLayout(false);
+            this.panelName.PerformLayout();
+            this.panelResults.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panelQuestions.ResumeLayout(false);
+            this.panelQuestions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        // Панели
+        private System.Windows.Forms.Panel panelName;
+        private System.Windows.Forms.Panel panelQuestions;
+        private System.Windows.Forms.Panel panelResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStartTest;
+
+        // Элементы panelName
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnShowResults;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSubmitAnswer;
+    }
+}
+
---
+++ GeniusOrNot_Windows_Forms/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeniusOrNot_Windows_Forms
+{
+    public partial class Form1 : Form
+    {
+        private string filePath = "results.csv";
+        private int totalQuestions = 5;
+        private string[] questions;
+        private int[] answers;
+        private int correctAnswers = 0;
+        private int currentQuestion = 0;
+        private List<int> usedQuestions = new List<int>();
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFile();
+            questions = GetQuestions();
+            answers = GetAnswers();
+            SetInitialState();
+            panelQuestions.Visible = false;
+            panelResults.Visible = false;
+            panelName.Visible = true;
+        }
+        private void SetInitialState()
+        {
+            panelQuestions.Visible = false;
+            panelResults.Visible = false;
+        }
+        private void InitializeFile()
+        {
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "ФИО;Правильные ответы;Диагноз\n");
+            }
+        }
+        private string[] GetQuestions() => new string[]
+{
+            "Сколько будет два плюс два умноженное на два?",
+            "Бревно нужно распилить на 10 частей. Сколько распилов нужно сделать?",
+            "На двух руках 10 пальцев. Сколько пальцев на 5 руках?",
+            "Укол делают каждые полчаса. Сколько нужно минут для трех уколов?",
+            "Пять свечей горело, две потухли. Сколько свечей осталось?"
+        };
+
+        private int[] GetAnswers() => new int[] { 6, 9, 25, 60, 2 };
+
+        private void btnStartTest_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Введите ФИО!");
+                return;
+            }
+
+            panelName.Visible = false;
+            panelQuestions.Visible = true;
+            correctAnswers = 0;
+            currentQuestion = 0;
+            usedQuestions.Clear();
+            ShowNextQuestion();
+        }
+
+        private void ShowNextQuestion()
+        {
+
+
+            if (currentQuestion >= totalQuestions)
+            {
+                FinishTest();
+                return;
+            }
+
+            int index;
+            do { index = random.Next(totalQuestions); }
+            while (usedQuestions.Contains(index));
+
+            usedQuestions.Add(index);
+            lblQuestion.Text = $"Вопрос {currentQuestion + 1}:\n{questions[index]}";
+            txtAnswer.Clear();
+            currentQuestion++;
+        }
+
+        private void btnSubmitAnswer_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtAnswer.Text, out int userAnswer))
+            {
+                MessageBox.Show("Введите число!");
+                return;
+            }
+
+            int correct = answers[usedQuestions[usedQuestions.Count - 1]];
+            if (userAnswer == correct) correctAnswers++;
+
+            ShowNextQuestion();
+        }
+        private void FinishTest()
+        {
+            double score = (double)correctAnswers / totalQuestions * 100;
+            string diagnosis = GetDiagnosis(score);
+
+            File.AppendAllText(filePath,
+                $"{txtName.Text};{correctAnswers};{diagnosis}\n");
+
+            panelQuestions.Visible = false;
+            panelResults.Visible = true;
+            LoadResults();
+            MessageBox.Show($"Тест завершен! Ваш диагноз: {diagnosis}");
+        }
+        private string GetDiagnosis(double score)
+        {
+            if (score >= 90) return "гений";
+            if (score >= 80) return "талант";
+            if (score >= 60) return "нормальный";
+            if (score >= 40) return "дурак";
+            if (score >= 20) return "идиот";
+            return "кретин";
+        }
+        private void LoadResults()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            // Создаем колонки вручную
+            dataGridView1.Columns.Add("ФИО", "ФИО");
+            dataGridView1.Columns.Add("ПравильныеОтветы", "Правильные ответы");
+            dataGridView1.Columns.Add("Диагноз", "Диагноз");
+
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var lines = File.ReadAllLines(filePath);
+                    for (int i = 1; i < lines.Length; i++) // Пропускаем заголовок
+                    {
+                        var parts = lines[i].Split(';');
+                        dataGridView1.Rows.Add(parts[0], parts[1], parts[2]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке результатов: {ex.Message}");
+            }
+        }
+
+        private void btnShowResults_Click(object sender, EventArgs e)
+        {
+            panelName.Visible = false;
+            panelQuestions.Visible = false;
+            panelResults.Visible = true;
+            LoadResults();
+        }
+
+    }
+}
